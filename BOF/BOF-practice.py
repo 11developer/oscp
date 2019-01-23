678bb82a220b73e53b6727aeb6d34fe6b015b9c9
Eva Prokofiev <44981946+Eva-Prokofiev@users.noreply.github.com>
2019-01-23 12:17:14 +0200

    Update and rename BOF-practice.txt to BOF-practice.py

---
+++ BOF/BOF-practice.py
@@ -0,0 +1,18 @@
+Buffer Overflows
+
+#EIP OVERWRITE 
+http://www.primalsecurity.net/0x0-exploit-tutorial-buffer-overflow-vanilla-eip-overwrite-2/
+
+#Exploiting "Vulnerable Server" for Windows 7
+https://samsclass.info/127/proj/vuln-server.htm
+    
+#Manually checking for Bad Characters
+http://www.bulbsecurity.com/finding-bad-characters-with-immunity-debugger-and-mona-py/
+    
+#Nice BOF write-ups
+Vulnhub VM:https://www.vulnhub.com/entry/brainpan-1,51/ 
+Write-up:https://blog.vonhewitt.com/2017/11/brainpan1/ 
+Youtube:https://www.youtube.com/watch?v=ohCY8CD6mSs (3 part series)
+
+#SANS training -BOF 
+https://www.sans.org/reading-room/whitepapers/threats/paper/481
