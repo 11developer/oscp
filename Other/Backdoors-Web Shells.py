02e4b547d5ef5aa531ec90947c0a0bf1efbe7a6d
Eevee <44981946+Eva-Prokofiev@users.noreply.github.com>
2019-01-24 13:35:04 +0200

    Update Backdoors-Web Shells.py

--- Other/Backdoors-Web Shells.py
+++ Other/Backdoors-Web Shells.py
@@ -1,12 +1,13 @@
 #Shells 
 
+http://pentestmonkey.net/cheat-sheet/shells/reverse-shell-cheat-sheet
 https://resources.infosecinstitute.com/icmp-reverse-shell/#gref
 
-    https://highon.coffee/blog/reverse-shell-cheat-sheet/
-    http://pentestmonkey.net/tools/web-shells/php-reverse-shell
-    http://pentestmonkey.net/tools/web-shells/perl-reverse-shell
-    https://github.com/BlackArch/webshells
-    https://github.com/tennc/webshell/tree/master/php/b374k
-    https://github.com/tennc/webshell/tree/master/php/PHPshell/c99shell
-    http://www.acunetix.com/blog/articles/web-shells-101-using-php-introduction-web-shells-part-2/
-    http://securityweekly.com/2011/10/23/python-one-line-shell-code/
+https://highon.coffee/blog/reverse-shell-cheat-sheet/
+http://pentestmonkey.net/tools/web-shells/php-reverse-shell
+http://pentestmonkey.net/tools/web-shells/perl-reverse-shell
+https://github.com/BlackArch/webshells
+https://github.com/tennc/webshell/tree/master/php/b374k
+https://github.com/tennc/webshell/tree/master/php/PHPshell/c99shell
+http://www.acunetix.com/blog/articles/web-shells-101-using-php-introduction-web-shells-part-2/
+http://securityweekly.com/2011/10/23/python-one-line-shell-code/
