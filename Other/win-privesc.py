6745b37940b729c7a9c9949f0424513e6a6f9d95
Eevee <44981946+Eva-Prokofiev@users.noreply.github.com>
2019-01-24 11:46:57 +0200

    Update win-privesc.py

--- Other/win-privesc.py
+++ Other/win-privesc.py
@@ -1,1 +1,15 @@
 
+# WINDOWS TESTS
+
+https://github.com/codingo/OSCP-2/blob/master/Windows/WinPrivCheck.bat
+Comprehensive Windows privesc script that checks KBs as well as common misconfigurations
+        
+https://github.com/foxglovesec/RottenPotato
+Local Privilege Escalation from Windows Service Accounts to SYSTEM
+
+https://github.com/PowerShellMafia/PowerSploit
+PowerSploit - A PowerShell Post-Exploitation Framework
+
+#Other nice write-ups for windows priv-esc 
+https://toshellandback.com/2015/11/24/ms-priv-esc/
+https://pentest.blog/windows-privilege-escalation-methods-for-pentesters/
