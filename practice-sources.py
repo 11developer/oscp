49f85889326101a2bf1dd9130b563e61b367be7f
Eevee <44981946+Eva-Prokofiev@users.noreply.github.com>
2019-01-24 10:59:21 +0200

    Update practice-sources.py

--- practice-sources.py
+++ practice-sources.py
@@ -227,3 +227,25 @@ https://scriptdotsh.com/index.php/2018/04/17/31-days-of-oscp-experience/
 https://www.lewisecurity.com/i-am-finally-an-oscp/ 
 
 https://prasannakumar.in/infosec/my-walk-towards-cracking-oscp/ 
+        
+        
+        
+        
+        Buffer Overflows
+
+#EIP OVERWRITE 
+http://www.primalsecurity.net/0x0-exploit-tutorial-buffer-overflow-vanilla-eip-overwrite-2/
+
+#Exploiting "Vulnerable Server" for Windows 7
+https://samsclass.info/127/proj/vuln-server.htm
+    
+#Manually checking for Bad Characters
+http://www.bulbsecurity.com/finding-bad-characters-with-immunity-debugger-and-mona-py/
+    
+#Nice BOF write-ups
+Vulnhub VM:https://www.vulnhub.com/entry/brainpan-1,51/ 
+Write-up:https://blog.vonhewitt.com/2017/11/brainpan1/ 
+Youtube:https://www.youtube.com/watch?v=ohCY8CD6mSs (3 part series)
+
+#SANS training -BOF 
+https://www.sans.org/reading-room/whitepapers/threats/paper/481
